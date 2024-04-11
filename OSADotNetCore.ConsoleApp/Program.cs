4308d2a96c8f5f2550e38d9d072a583962191d31
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-04-11 13:43:35 +0630

    ADO.Net CRUD

--- OSADotNetCore.ConsoleApp/Program.cs
+++ OSADotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OSADotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("Title", "Author", "Content");
+//adoDotNetExample.Update(12, "Test Title", "Test Author", "Test Content");
+//adoDotNetExample.Delete(12);
+adoDotNetExample.Edit(10);
+Console.ReadKey();
